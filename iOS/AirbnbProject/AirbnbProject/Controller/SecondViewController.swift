a1f0eb0004f2466a7f7cbde823af170945296da1
dev-Lena <keunnalee@gmail.com>
2020-05-19 22:39:44 +0900

    [iOS] feat: 임시 Model 구조체 생성
    Issue #6

--- iOS/AirbnbProject/AirbnbProject/Controller/SecondViewController.swift
+++ iOS/AirbnbProject/AirbnbProject/Controller/SecondViewController.swift
@@ -12,9 +12,6 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
