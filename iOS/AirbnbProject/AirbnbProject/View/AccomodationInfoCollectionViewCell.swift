8cc89c91c1afc0d46ca6c850cbbb0c898012c6f9
limwin94 <amuguna94@gmail.com>
2020-05-19 15:50:12 +0900

    [iOS] feat: 메인화면 오토레이아웃 설정
    상단 검색창, 필터 버튼, 안내문구 및 숙소 collection view 오토레이아웃 설정
    Issue #6

---
+++ iOS/AirbnbProject/AirbnbProject/View/AccomodationInfoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AccomodationInfoCollectionViewCell.swift
+//  AirbnbProject
+//
+//  Created by 임승혁 on 2020/05/19.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class AccomodationInfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
