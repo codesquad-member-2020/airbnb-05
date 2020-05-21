4ca76066883686eecaf306ea4c8528051e636724
dev-Lena <keunnalee@gmail.com>
2020-05-22 00:17:17 +0900

    [iOS] feat: ExtentionButton 클래스 생성
    - UIButton Extension

---
+++ iOS/AirbnbProject/AirbnbProject/View/ExtensionButton.swift
@@ -0,0 +1,21 @@
+//
+//  ExtensionButton.swift
+//  AirbnbProject
+//
+//  Created by Keunna Lee on 2020/05/21.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class ExtensionButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
