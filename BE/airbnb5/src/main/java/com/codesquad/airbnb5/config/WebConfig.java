cb4141237fac51d5e88be31a860a833358ad1443
suhyunsim <cnc1106@naver.com>
2020-05-22 02:31:50 +0900

    [BE] feat: CORS 방지를 위한 config 파일 추가
    Issue #18

---
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.codesquad.airbnb5.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        long MAX_AGE_SECS = 3600;
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
