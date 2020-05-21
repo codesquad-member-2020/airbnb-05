168e80f783738516520ccc8bac0d42739a3b60b7
dev-Lena <keunnalee@gmail.com>
2020-05-21 16:50:44 +0900

    [iOS] feat: 메인화면 TableView에 UITapGestureRecognizer 추가
    Issue #11

--- iOS/AirbnbProject/AirbnbProject/Controller/MainViewController.swift
+++ iOS/AirbnbProject/AirbnbProject/Controller/MainViewController.swift
@@ -12,6 +12,7 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var infoTableView: UITableView!
     private let tableViewHeight = CGFloat(250)
+
     var models = [Model]()
     
     override func viewDidLoad() {
@@ -21,18 +22,25 @@ class MainViewController: UIViewController {
         models.append(Model(imageName: "bye"))
         models.append(Model(imageName: "bye"))
         models.append(Model(imageName: "bye"))
-        infoTableView.delaysContentTouches = false
     }
     
     private func setupTableView() {
         infoTableView.register(AccomodationInfoTableViewCell.nib(), forCellReuseIdentifier: AccomodationInfoTableViewCell.identifier)
         infoTableView.dataSource = self
         infoTableView.delegate = self
+        infoTableView.delaysContentTouches = false
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(accomodationCellTapped))
+        self.infoTableView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func accomodationCellTapped(_ sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: self.infoTableView)
+        guard let tappedCellIndexPath = self.infoTableView.indexPathForRow(at: tapLocation) else {return}
+        let tappedCell = self.infoTableView.cellForRow(at: tappedCellIndexPath) as! AccomodationInfoTableViewCell
     }
 }
 
-extension MainViewController: UITableViewDataSource, UITableViewDelegate {
-    
+extension MainViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
@@ -42,10 +50,11 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
         cell.configure(with: models)
         return cell
     }
-    
+}
+
+extension MainViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return self.tableViewHeight
     }
 }
-
 
