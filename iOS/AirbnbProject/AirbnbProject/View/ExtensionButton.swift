0f347fed50177d125a61637b75ecb9317be15ab0
dev-Lena <keunnalee@gmail.com>
2020-05-22 00:18:27 +0900

    [iOS] feat: Button 애니메이션 추가
    - UIButton Extension
    #Issue 11,13

--- iOS/AirbnbProject/AirbnbProject/View/ExtensionButton.swift
+++ iOS/AirbnbProject/AirbnbProject/View/ExtensionButton.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class ExtensionButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+extension UIButton {
+    
+    func startAnimatingPressActions() {
+        addTarget(self, action: #selector(animateDown), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(animateUp), for: [.touchDragExit, .touchCancel, .touchUpInside, .touchUpOutside])
     }
-    */
-
+    
+    @objc private func animateDown(sender: UIButton) {
+        animate(sender, transform: CGAffineTransform.identity.scaledBy(x: 0.95, y: 0.95))
+    }
+    
+    @objc private func animateUp(sender: UIButton) {
+        animate(sender, transform: .identity)
+    }
+    
+    private func animate(_ button: UIButton, transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.4,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 3,
+                       options: [.curveEaseInOut],
+                       animations: {
+                        button.transform = transform
+            }, completion: nil)
+    }
 }
