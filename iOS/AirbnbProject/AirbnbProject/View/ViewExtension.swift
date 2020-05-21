f62fb1dd0bfabdf617090932c7425eb03b2cbe59
dev-Lena <keunnalee@gmail.com>
2020-05-21 23:20:48 +0900

    [iOS] feat: UIView Extension 클래스 생성
    Issue #11, #13

---
+++ iOS/AirbnbProject/AirbnbProject/View/ViewExtension.swift
@@ -0,0 +1,21 @@
+//
+//  ViewExtension.swift
+//  AirbnbProject
+//
+//  Created by Keunna Lee on 2020/05/21.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class ViewExtension: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
