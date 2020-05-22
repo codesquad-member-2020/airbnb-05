fc825e1e7bf20b3f17e2ab48bc5a54bddf64261a
limwin94 <amuguna94@gmail.com>
2020-05-22 16:49:39 +0900

    [iOS] feat: plus, minus button을 동작하여 count가 올라가고 내려가도록 함.
    Issue #14

--- iOS/AirbnbProject/AirbnbProject/View/GuestFilterViewController.swift
+++ iOS/AirbnbProject/AirbnbProject/View/GuestFilterViewController.swift
@@ -13,7 +13,33 @@ class GuestFilterViewController: UIViewController {
     @IBOutlet weak var youthCountFilter: GuestFilterReusableView!
     @IBOutlet weak var infantCountFilter: GuestFilterReusableView!
     
+    private var totalCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonAddTarget(view: adultCountFilter)
+        buttonAddTarget(view: youthCountFilter)
+        buttonAddTarget(view: infantCountFilter)
+    }
+    
+    private func buttonAddTarget(view: GuestFilterReusableView) {
+        view.minusButton.addTarget(self, action: #selector(self.minusButtonTouchAction), for: .touchUpInside)
+        view.plusButton.addTarget(self, action: #selector(self.plusButtonTouchAction), for: .touchUpInside)
+    }
+    
+    @objc private func plusButtonTouchAction(sender: UIButton) {
+        guard let view = sender.superview?.superview?.superview as? GuestFilterReusableView else { return }
+        let currentCount = Int(view.guestCount.text!)
+        view.guestCount.text = String(currentCount! + 1)
+        
+        totalCount += 1
+    }
+    
+    @objc private func minusButtonTouchAction(sender: UIButton) {
+        guard let view = sender.superview?.superview?.superview as? GuestFilterReusableView else { return }
+        let currentCount = Int(view.guestCount.text!)
+        view.guestCount.text = String(currentCount! - 1)
+        
+       totalCount -= 1
     }
 }
