c8df752e8411d83a903c745f5e5e32153b6bf72e
dev-Lena <keunnalee@gmail.com>
2020-05-23 21:06:28 +0900

    [iOS] feat: 캘린더 스켈레톤 CollectionView 구현
    Issue #21

---
+++ iOS/AirbnbProject/AirbnbProject/View/CalendarView/DayCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DayCollectionViewCell.swift
+//  AirbnbProject
+//
+//  Created by Keunna Lee on 2020/05/23.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class DayCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    static let identifier = "DayCollectionViewCell"
+}
