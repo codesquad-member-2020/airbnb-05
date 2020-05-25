5f16c0446f5ff844f14774a93e52bc38a9668f03
suhyunsim <cnc1106@naver.com>
2020-05-25 20:00:56 +0900

    [BE] feat: 페이징을 위한 api 구현
    RoomDao에 limit과 offset을 이용해 페이징 기능을 처리하는 findRooms를 구현한다.
    
    Issue #23

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/RoomDao.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/RoomDao.java
@@ -19,6 +19,35 @@ public class RoomDao {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    public Object findRooms(
+            int cityId,
+            int limit,
+            int offset
+    ) {
+        String sql = "SELECT r.room_id, r.room_name, r.room_thumbnail, h.is_super_host, " +
+                "r.room_type, r.beds, r.scores, r.reviews " +
+                "FROM room r " +
+                "JOIN host h " +
+                "ON r.host_id = h.host_id " +
+                "WHERE r.city_id = ? " +
+                "LIMIT ? " +
+                "OFFSET ? ";
+
+        RowMapper<Object> roomScrollRowMapper = (rs, rowNum) -> {
+            RoomDto roomDto = new RoomDto.Builder(rs.getInt("room_id"))
+                    .roomName(rs.getString("room_name"))
+                    .roomThumbnail(rs.getString("room_thumbnail"))
+                    .isSuperHost(rs.getBoolean("is_super_host"))
+                    .roomType(rs.getString("room_type"))
+                    .beds(rs.getInt("beds"))
+                    .scores(rs.getFloat("scores"))
+                    .reviews(rs.getFloat("reviews"))
+                    .build();
+            return roomDto;
+        };
+        return jdbcTemplate.query(sql, new Object[]{cityId, limit, offset}, roomScrollRowMapper);
+    }
+
     public Object findRoomSummary(
             int cityId,
             int guests,
@@ -30,11 +59,9 @@ public class RoomDao {
         String sql = "SELECT r.room_id, r.room_name, r.room_thumbnail, h.is_super_host, " +
                 "r.room_type, r.beds, r.scores, r.reviews " +
                 "FROM room r " +
-                "JOIN city c " +
-                "ON c.city_id = r.city_id " +
                 "JOIN host h " +
                 "ON r.host_id = h.host_id " +
-                "WHERE c.city_id = ? " +
+                "WHERE r.city_id = ? " +
                 "AND r.maximum_guests >= ? " +
                 "AND r.sale_price BETWEEN ? AND ? " +
                 "AND r.room_id NOT IN (" +
