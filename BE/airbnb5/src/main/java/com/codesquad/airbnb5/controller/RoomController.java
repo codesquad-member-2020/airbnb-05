6cefe631155b5e3ad16860b21b6893eb232bc849
suhyunsim <cnc1106@naver.com>
2020-05-25 18:13:42 +0900

    [BE] feat: RoomController 날짜 조건 requestParameter로 전달 받도록 구현
    Issue #23

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/controller/RoomController.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/controller/RoomController.java
@@ -3,11 +3,14 @@ package com.codesquad.airbnb5.controller;
 import com.codesquad.airbnb5.dto.ResponseDto;
 import com.codesquad.airbnb5.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class RoomController {
@@ -20,9 +23,13 @@ public class RoomController {
             @PathVariable("cityId") int cityId,
             @RequestParam(value = "guests", required = false, defaultValue = "0") int guests,
             @RequestParam(value = "minPrice", required = false, defaultValue = "0") int minPrice,
-            @RequestParam(value = "maxPrice", required = false, defaultValue = "1400000") int maxPrice) {
+            @RequestParam(value = "maxPrice", required = false, defaultValue = "1400000") int maxPrice,
+            @RequestParam(value = "checkIn", required = false, defaultValue = "1900-05-20")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkIn,
+            @RequestParam(value = "checkOut", required = false, defaultValue = "2022-05-20")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOut) {
 
-        ResponseDto responseDto = roomService.getRoomSummary(cityId, guests, minPrice, maxPrice);
+        ResponseDto responseDto = roomService.getRoomSummary(cityId, guests, minPrice, maxPrice, checkIn, checkOut);
         return ResponseEntity.ok().body(responseDto);
     }
 }
