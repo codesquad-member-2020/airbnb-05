e352a10eface2dde88e6438679c30a783101405f
suhyunsim <cnc1106@naver.com>
2020-05-25 18:09:43 +0900

    [BE] feat: RoomDao에 날짜 필터링 조건 추가
    예약된 날짜를 제외하는 쿼리를 작성해 숙소를 필터링한다.
    
    Issue #23

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/RoomDao.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/RoomDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.time.LocalDate;
 
 @Repository
 public class RoomDao {
@@ -22,7 +23,9 @@ public class RoomDao {
             int cityId,
             int guests,
             int minPrice,
-            int maxPrice
+            int maxPrice,
+            LocalDate checkIn,
+            LocalDate checkOut
     ) {
         String sql = "SELECT r.room_id, r.room_name, r.room_thumbnail, h.is_super_host, " +
                 "r.room_type, r.beds, r.scores, r.reviews " +
@@ -33,8 +36,14 @@ public class RoomDao {
                 "ON r.host_id = h.host_id " +
                 "WHERE c.city_id = ? " +
                 "AND r.maximum_guests >= ? " +
-                "AND r.sale_price BETWEEN ? AND ?";
-
+                "AND r.sale_price BETWEEN ? AND ? " +
+                "AND r.room_id NOT IN (" +
+                "SELECT re.room_id " +
+                "FROM reservation re " +
+                "WHERE (check_in >= ? AND check_out <= ?) " +
+                "OR (check_in <= ? AND check_out >= ?) " +
+                "OR (check_in <= ? AND check_out > ?) " +
+                "OR (check_out > ? AND check_in < ?))";
         RowMapper<Object> roomSummaryRowMapper = (rs, rowNum) -> {
             RoomDto roomDto = new RoomDto.Builder(rs.getInt("room_id"))
                     .roomName(rs.getString("room_name"))
@@ -47,6 +56,6 @@ public class RoomDao {
                     .build();
             return roomDto;
         };
-        return jdbcTemplate.query(sql, new Object[]{cityId, guests, minPrice, maxPrice}, roomSummaryRowMapper);
+        return jdbcTemplate.query(sql, new Object[]{cityId, guests, minPrice, maxPrice, checkIn, checkOut, checkIn, checkOut, checkIn, checkIn, checkOut, checkOut}, roomSummaryRowMapper);
     }
 }
