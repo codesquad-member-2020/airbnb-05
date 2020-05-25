570f5779d89012a3b0affbf7ea1cce44c638fa11
suhyunsim <cnc1106@naver.com>
2020-05-25 18:11:49 +0900

    [BE] feat: RoomService에 날짜 조건 매개변수 추가
    Issue #23

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/RoomService.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/RoomService.java
@@ -5,14 +5,16 @@ import com.codesquad.airbnb5.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class RoomService {
 
     @Autowired
     private RoomDao roomDao;
 
-    public ResponseDto getRoomSummary(int cityId, int guests, int minPrice, int maxPrice) {
-        Object roomDto = roomDao.findRoomSummary(cityId, guests, minPrice, maxPrice);
+    public ResponseDto getRoomSummary(int cityId, int guests, int minPrice, int maxPrice, LocalDate checkIn, LocalDate checkOut) {
+        Object roomDto = roomDao.findRoomSummary(cityId, guests, minPrice, maxPrice, checkIn, checkOut);
         return new ResponseDto(200, roomDto);
     }
 
