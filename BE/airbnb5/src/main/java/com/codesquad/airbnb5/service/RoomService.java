e2d9a4e2052d2458f7f80fd12634488e27fe8e51
suhyunsim <cnc1106@naver.com>
2020-05-25 20:03:22 +0900

    [BE] feat: RoomService에 페이징 기능을 수행하는 getRoomScroll 추가
    Issue #23

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/RoomService.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/RoomService.java
@@ -13,6 +13,11 @@ public class RoomService {
     @Autowired
     private RoomDao roomDao;
 
+    public ResponseDto getRoomScroll(int cityId, int limit, int offset) {
+        Object roomDto = roomDao.findRooms(cityId, limit, offset);
+        return new ResponseDto(200, roomDto);
+    }
+
     public ResponseDto getRoomSummary(int cityId, int guests, int minPrice, int maxPrice, LocalDate checkIn, LocalDate checkOut) {
         Object roomDto = roomDao.findRoomSummary(cityId, guests, minPrice, maxPrice, checkIn, checkOut);
         return new ResponseDto(200, roomDto);
