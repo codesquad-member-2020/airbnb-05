0a005d10ca9151b2a096c74dffcdcb7dd6dbab8c
suhyunsim <cnc1106@naver.com>
2020-05-29 02:52:48 +0900

    [BE] feat: findBookmarkRoom 메서드 구현
    guestId와 roomId를 통해 게스트가 등록한 숙소의 간략 정보를 제공하는 기능을 구현한다.
    즐겨찾기 여부는 findFavoriteByGuestIdAndRoomId를 통해 boolean으로 확인한다.
    
    Issue #44

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/BookmarkDao.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/BookmarkDao.java
@@ -1,7 +1,9 @@
 package com.codesquad.airbnb5.dao;
 
+import com.codesquad.airbnb5.dto.RoomDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
@@ -29,6 +31,34 @@ public class BookmarkDao {
         jdbcTemplate.update(sql, roomId, guestId);
     }
 
+    public Object findBookmarkRoom(int guestId, int roomId) {
+
+        String sql = "SELECT r.room_id, r.room_name, r.room_thumbnail, h.is_super_host, " +
+                "r.room_type, r.beds, r.scores, r.reviews " +
+                "FROM room r " +
+                "JOIN host h " +
+                "ON r.host_id = h.host_id " +
+                "JOIN bookmark b " +
+                "ON r.room_id = b.room_id " +
+                "WHERE b.guest_id = ? " +
+                "AND b.room_id = ?";
+
+        RowMapper<Object> bookmarkRowMapper = (rs, rowNum) -> {
+            RoomDto roomDto = new RoomDto.Builder(rs.getInt("room_id"))
+                    .roomName(rs.getString("room_name"))
+                    .roomThumbnail(rs.getString("room_thumbnail"))
+                    .isSuperHost(rs.getBoolean("is_super_host"))
+                    .roomType(rs.getString("room_type"))
+                    .beds(rs.getInt("beds"))
+                    .scores(rs.getFloat("scores"))
+                    .reviews(rs.getFloat("reviews"))
+                    .isFavorite(findFavoriteByGuestIdAndRoomId(guestId, roomId))
+                    .build();
+            return roomDto;
+        };
+
+        return jdbcTemplate.queryForObject(sql, new Object[]{guestId, roomId}, bookmarkRowMapper);
+    }
 
     private Boolean findFavoriteByGuestIdAndRoomId(int guestId, int roomId) {
 
