86cebf90f8bf32137f8fc2fbc0ba60d90cc110bc
suhyunsim <cnc1106@naver.com>
2020-05-31 01:20:41 +0900

    [BE] feat: GlobalExceptionHandler 추가
    NotFoundException을 핸들한다.
    
    Issue #24

---
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.codesquad.airbnb5.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+}
