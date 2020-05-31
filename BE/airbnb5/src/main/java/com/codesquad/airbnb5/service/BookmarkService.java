7926af7fee9237d50c2e1f1f379258abc38fc116
suhyunsim <cnc1106@naver.com>
2020-05-31 15:03:51 +0900

    [BE] refactor: roomMapper를 파라미터로 사용하기 위해 추가

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/BookmarkService.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/BookmarkService.java
@@ -1,11 +1,13 @@
 package com.codesquad.airbnb5.service;
 
 import com.codesquad.airbnb5.dao.BookmarkDao;
+import com.codesquad.airbnb5.dao.RoomMapper;
 import com.codesquad.airbnb5.domain.BookmarkRepository;
 import com.codesquad.airbnb5.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,11 +29,11 @@ public class BookmarkService {
         return new ResponseDto(200);
     }
 
-    public ResponseDto getBookmarkList(int guestId) {
+    public ResponseDto getBookmarkList(int guestId, RoomMapper roomMapper) throws SQLException {
         List<Object> bookmarks = new ArrayList<>();
         List<Integer> roomList = bookmarkRepository.findBookmarkRoomByGuestId(guestId);
         for (int roomId : roomList) {
-            Object roomDto = bookmarkDao.findBookmarkRoom(guestId, roomId);
+            Object roomDto = bookmarkDao.findBookmarkRoom(guestId, roomId, roomMapper);
             bookmarks.add(roomDto);
         }
         return new ResponseDto(200, bookmarks);
--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/RoomService.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/RoomService.java
@@ -1,10 +1,12 @@
 package com.codesquad.airbnb5.service;
 
 import com.codesquad.airbnb5.dao.RoomDao;
+import com.codesquad.airbnb5.dao.RoomMapper;
 import com.codesquad.airbnb5.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 @Service
@@ -13,13 +15,13 @@ public class RoomService {
     @Autowired
     private RoomDao roomDao;
 
-    public ResponseDto getRoomScroll(int cityId, int limit, int offset) {
-        Object roomDto = roomDao.findRooms(cityId, limit, offset);
+    public ResponseDto getRoomScroll(int cityId, int limit, int offset, int guestId, RoomMapper roomMapper) throws SQLException {
+        Object roomDto = roomDao.findRooms(cityId, limit, offset, guestId, roomMapper);
         return new ResponseDto(200, roomDto);
     }
 
-    public ResponseDto getRoomSummary(int cityId, int guests, int minPrice, int maxPrice, LocalDate checkIn, LocalDate checkOut) {
-        Object roomDto = roomDao.findRoomSummary(cityId, guests, minPrice, maxPrice, checkIn, checkOut);
+    public ResponseDto getRoomSummary(int cityId, int guests, int minPrice, int maxPrice, LocalDate checkIn, LocalDate checkOut, int guestId, RoomMapper roomMapper) throws SQLException {
+        Object roomDto = roomDao.findRoomSummary(cityId, guests, minPrice, maxPrice, checkIn, checkOut, guestId, roomMapper);
         return new ResponseDto(200, roomDto);
     }
 
