54717c88c63ca00176f9fe8f58cfee9028387abb
suhyunsim <cnc1106@naver.com>
2020-05-31 22:56:19 +0900

    [BE] feat: RoomDetailDto를 반환하는 RowMappper 구현
    숙소 상세 정보를 제공하기 위해 RowMapper인 mapRowDetail을 구현한다.
    로그인한 유저에 따라 즐겨찾기 여부를 제공한다.
    숙소 별 상세 이미지들을 리스트로 제공하기 위해 findDetailImage 메서드를 구현한다.
    
    Issue #25

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/RoomMapper.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/RoomMapper.java
@@ -1,5 +1,6 @@
 package com.codesquad.airbnb5.dao;
 
+import com.codesquad.airbnb5.dto.RoomDetailDto;
 import com.codesquad.airbnb5.dto.RoomDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Slf4j
 @Repository
@@ -44,6 +46,37 @@ public class RoomMapper {
         };
     }
 
+    public RowMapper<RoomDetailDto> mapRowDetail(int guestId) throws SQLException {
+        return new RowMapper<RoomDetailDto>() {
+            @Override
+            public RoomDetailDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                int roomId = rs.getInt("room_id");
+                boolean bool = findFavoriteByGuestIdAndRoomId(guestId, roomId);
+
+                return RoomDetailDto.builder()
+                        .roomId(rs.getInt("r.room_id"))
+                        .roomName(rs.getString("room_name"))
+                        .address(rs.getString("address"))
+                        .roomThumbnail(rs.getString("room_thumbnail"))
+                        .detailImages(findDetailImage(roomId))
+                        .isSuperHost(rs.getBoolean("is_super_host"))
+                        .hostName(rs.getString("host_name"))
+                        .hostThumbnail(rs.getString("host_thumbnail"))
+                        .roomType(rs.getString("room_type"))
+                        .beds(rs.getInt("beds"))
+                        .originalPrice(rs.getInt("original_price"))
+                        .salePrice(rs.getInt("sale_price"))
+                        .scores(rs.getFloat("scores"))
+                        .reviews(rs.getFloat("reviews"))
+                        .amenities(rs.getString("amenities"))
+                        .cleaningFee(rs.getInt("cleaning_fee"))
+                        .maximumGuests(rs.getInt("maximum_guests"))
+                        .isFavorite(bool)
+                        .build();
+            }
+        };
+    }
+
     private Boolean findFavoriteByGuestIdAndRoomId(int guestId, int roomId) {
 
         String sql = "SELECT exists ( SELECT room_id " +
@@ -51,5 +84,16 @@ public class RoomMapper {
                 "WHERE b.guest_id = ? " +
                 "AND b.room_id = ? )";
         return jdbcTemplate.queryForObject(sql, new Object[]{guestId, roomId}, Boolean.class);
+    }
+
+    private List<String> findDetailImage(int roomId) {
+
+        String sql = "SELECT d.detail_url " +
+                "FROM detail_image d " +
+                "WHERE room_id = ? ";
+
+        return jdbcTemplate.query(sql, new Object[]{roomId}, ((rs, rowNum) -> {
+            return rs.getString("detail_url");
+        }));
     }
 }
