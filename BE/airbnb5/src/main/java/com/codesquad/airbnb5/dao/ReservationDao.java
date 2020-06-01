4bb8934aad723e8f41f6f931d4cedfbd9dd1b683
suhyunsim <cnc1106@naver.com>
2020-06-02 02:20:56 +0900

    [BE] feat: ReservationDao에 접속자 별 예약 조회하기 기능 구현
    guestId를 파라미터로 예약한 숙소들의 정보를 제공한다.
    
    Issue #26

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/ReservationDao.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/ReservationDao.java
@@ -1,8 +1,10 @@
 package com.codesquad.airbnb5.dao;
 
 import com.codesquad.airbnb5.domain.RoomRepository;
+import com.codesquad.airbnb5.dto.ReservationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
@@ -36,10 +38,39 @@ public class ReservationDao {
                 roomRepository.findCleaningFeeByRoomId(roomId),
                 roomRepository.findTaxByRoomId(roomId));
     }
-    
+
     public void deleteReservation(int reservationId) {
         String sql = "DELETE FROM reservation " +
                 "WHERE reservation_id = ? ";
         jdbcTemplate.update(sql, reservationId);
+    }
+
+    public Object findReservationList(int guestId) {
+        String sql = "SELECT reservation_id, guest_id, room_id, room_name, room_type, original_price, sale_price, " +
+                "scores, reviews, check_in, check_out, nights, guests, cleaning_fee, tax, total_fee " +
+                "FROM reservation r " +
+                "WHERE guest_id = ? ";
+
+        RowMapper<Object> reservationRowMapper = (rs, rowNum) -> {
+            return ReservationDto.builder()
+                    .reservationId(rs.getInt("reservation_id"))
+                    .roomId(rs.getInt("room_id"))
+                    .roomName(rs.getString("room_name"))
+                    .roomType(rs.getString("room_type"))
+                    .originalPrice(rs.getInt("original_price"))
+                    .salePrice(rs.getInt("sale_price"))
+                    .scores(rs.getFloat("scores"))
+                    .reviews(rs.getInt("reviews"))
+                    .checkIn(rs.getDate("check_in").toLocalDate())
+                    .checkOut(rs.getDate("check_out").toLocalDate())
+                    .nights(rs.getInt("nights"))
+                    .guests(rs.getInt("guests"))
+                    .cleaningFee(rs.getInt("cleaning_fee"))
+                    .tax(rs.getInt("tax"))
+                    .totalFee(rs.getInt("total_fee"))
+                    .build();
+        };
+
+        return jdbcTemplate.query(sql, new Object[]{guestId}, reservationRowMapper);
     }
 }
