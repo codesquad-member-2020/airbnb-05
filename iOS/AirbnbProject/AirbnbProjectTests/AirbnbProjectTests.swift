32674531ecdd92fd91f3513aefa2ea2d1bcbc3c8
dev-Lena <keunnalee@gmail.com>
2020-06-03 22:28:05 +0900

    [iOS] feat: NetworkManager 객체 테스트 케이스 메소드 추가
    Issue #56

--- iOS/AirbnbProject/AirbnbProjectTests/AirbnbProjectTests.swift
+++ iOS/AirbnbProject/AirbnbProjectTests/AirbnbProjectTests.swift
@@ -10,25 +10,40 @@ import XCTest
 @testable import AirbnbProject
 
 class AirbnbProjectTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    func testRequestCityNames() throws {
+        let networkManager = NetworkManager()
+        
+        let expectation = XCTestExpectation(description: "URL - 성공")
+        defer { wait(for: [expectation], timeout:  5.0) }
+        
+        networkManager.requestData(url: EndPoints.requestCityList, method: .get, body: nil) { (data, response, error) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            
+            XCTAssertEqual(response?.statusCode, 200)
+            expectation.fulfill()
+        }
+    }
 }
