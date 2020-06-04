c1f6ebd33745802fba7611fa609b5ea69cc53e16
suhyunsim <cnc1106@naver.com>
2020-06-04 14:38:29 +0900

    [BE] feat: LoginController 구현
    Github OAuth 로그인 성공 후 유저의 정보를 DB에 저장하고 정의한 스킴에 토큰을 담는다.
    
    Issue #11

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/controller/LoginController.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.codesquad.airbnb5.controller;
 import com.codesquad.airbnb5.domain.User;
 import com.codesquad.airbnb5.service.JwtService;
 import com.codesquad.airbnb5.service.OAuthService;
+import com.codesquad.airbnb5.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 import java.io.IOException;
 
+@Slf4j
 @RestController
 public class LoginController {
 
     private final OAuthService oAuthService;
     private final JwtService jwtService;
+    private final UserService userService;
 
-    public LoginController(OAuthService oAuthService, JwtService jwtService) {
+    public LoginController(OAuthService oAuthService, JwtService jwtService, UserService userService) {
         this.oAuthService = oAuthService;
         this.jwtService = jwtService;
+        this.userService = userService;
     }
 
     @GetMapping("/githublogin")
     public ResponseEntity<String> githubLogin(@PathParam("code") String code, HttpServletResponse response) throws IOException {
         User user = oAuthService.getUser(code);
+        userService.insertOrUpdateUser(user);
         String jwt = jwtService.buildJwt(user);
         response.sendRedirect("airbnbfive://oauth?token=" + jwt);
         return ResponseEntity.ok("login 성공");
