23345843e1d84f64b7e7b6df98ef0d24cac9644b
suhyunsim <cnc1106@naver.com>
2020-06-04 14:24:44 +0900

    [BE] feat: UserDao 구현
    github api를 통해 얻어 온 유저의 정보를 유저 테이블에 저정하는 insertOrUpdateUser 메서드를 구현한다.
    동일한 유저가 로그인 할 경우 정보를 새로 저장하지 않기 위해 duplicate 쿼리를 사용한다.
    
    Issue #11

---
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/dao/UserDao.java
@@ -0,0 +1,36 @@
+package com.codesquad.airbnb5.dao;
+
+import com.codesquad.airbnb5.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class UserDao {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public void insertOrUpdateUser(User user) {
+        String sql = "INSERT INTO user (github_id, github_name, github_email) " +
+                "VALUES (:github_id, :github_name, :github_email) " +
+                "ON DUPLICATE KEY " +
+                "UPDATE github_name = :github_name, github_email = :github_email";
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue("github_id", user.getGithubId())
+                .addValue("github_name", user.getGithubName())
+                .addValue("github_email", user.getGithubEmail());
+        namedParameterJdbcTemplate.update(sql, sqlParameterSource);
+    }
+}
