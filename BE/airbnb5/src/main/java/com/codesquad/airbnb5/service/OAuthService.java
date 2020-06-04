0c8a05d5dacf385ce7d179b7f17c49d508240b67
suhyunsim <cnc1106@naver.com>
2020-06-04 14:35:08 +0900

    [BE] feat: OAuthService에 getAccessToken, getUser 메서드 구현
    토큰을 가져오는 메서드와 유저의 정보를 요청하는 메서드를 구현한다.
    
    Issue #11

--- BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/OAuthService.java
+++ BE/airbnb5/src/main/java/com/codesquad/airbnb5/service/OAuthService.java
@@ -6,15 +6,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Collections;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -29,45 +31,39 @@ public class OAuthService {
         this.objectMapper = objectMapper;
     }
 
+    public GithubToken getAccessToken(String code) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        Map<String, String> header = new HashMap<>();
+        header.put("Accept", "application/json");
+        headers.setAll(header);
+
+        MultiValueMap<String, String> requestPayloads = new LinkedMultiValueMap<>();
+        Map<String, String> requestPayload = new HashMap<>();
+        requestPayload.put("client_id", CLIENT_ID);
+        requestPayload.put("client_secret", CLIENT_SECRET);
+        requestPayload.put("code", code);
+        requestPayloads.setAll(requestPayload);
+
+        HttpEntity<?> request = new HttpEntity<>(requestPayloads, headers);
+        ResponseEntity<?> response = new RestTemplate().postForEntity(ACCESS_TOKEN_URL, request, GithubToken.class);
+        return (GithubToken) response.getBody();
+    }
+
     public User getUser(String code) throws JsonProcessingException {
-        String accessToken = getAccessToken(code);
+        GithubToken accessToken = getAccessToken(code);
         ResponseEntity<String> response = new RestTemplate().exchange(USER_DATA_API, HttpMethod.GET,
                 getHttpEntityWithAuthorization(accessToken), String.class);
         JsonNode jsonNode = objectMapper.readTree(response.getBody());
         return User.builder()
-                .id(jsonNode.required("id").asInt())
-                .name(jsonNode.required("name").asText())
-                .email(jsonNode.required("email").asText())
+                .githubId(jsonNode.required("id").asInt())
+                .githubName(jsonNode.required("name").asText())
+                .githubEmail(jsonNode.required("email").asText())
                 .build();
     }
 
-    private String getAccessToken(String code) {
-        GithubToken githubToken = new RestTemplate().postForEntity(getUrlForAccessToken(code),
-                getHttpEntityWithSetAcceptJson(), GithubToken.class)
-                .getBody();
-        return Objects.requireNonNull(githubToken).getAccessToken();
-    }
-
-    private String getUrlForAccessToken(String code) {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("client_id", CLIENT_ID);
-        params.add("client_secret", CLIENT_SECRET);
-        params.add("code", code);
-        return UriComponentsBuilder.fromHttpUrl(ACCESS_TOKEN_URL)
-                .queryParams(params)
-                .toUriString();
-    }
-
-    private HttpEntity<String> getHttpEntityWithSetAcceptJson() {
+    private HttpEntity<String> getHttpEntityWithAuthorization(GithubToken githubToken) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(githubToken.getAccessToken());
         return new HttpEntity<>(headers);
     }
-
-    private HttpEntity<String> getHttpEntityWithAuthorization(String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        return new HttpEntity<>(headers);
-    }
-
 }
